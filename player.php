6095cf27df521e655ed4b2e2c25de75d574605d6
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-10-12 15:30:35 +0200

    added comments and player file

---
+++ player.php
@@ -0,0 +1,53 @@
+<?php
+
+class Player
+{
+    private $cards = [];
+    private $lost = False;
+
+
+//hit
+
+    public function hit()
+    {
+
+    }
+}
+
+//surrender
+public function surrender ()
+{
+
+}
+
+//getScore
+public function getScore ()
+{
+
+}
+}
+//hasLost
+public function hasLoste ()
+{
+
+}
+}
+
+?>
+// todo google some basic OOP articles
+//todo First spent some time reading these classes and really understand what they are doing.
+
+// face card jack, king, or queen = 10
+// Ace = 11
+
+//  object = properties and methods (also called instances)
+/*  properties = variables and constants (classmember variables) (also called fields, attributes)
+    The properties hold specific data and related with the class in which it has been defined*/
+//  methods = functions
+
+// class is a construct from which objects are created
+//includes properties and methods (objects)
+// To create an object out of a class, the new keyword must be used.
+
+//method
+//A class method is exactly similar to PHP functions.
